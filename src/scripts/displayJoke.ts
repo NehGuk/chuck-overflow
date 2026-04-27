68d7ce7a4034c2b2fc88ba334b66ffa18a297e54
NehGuk <nurture.tumulus_0n@icloud.com>
2026-04-27 21:55:49 +0200

    Implements joke button functionality

--- src/scripts/displayJoke.ts
+++ src/scripts/displayJoke.ts
@@ -1,8 +1,12 @@
 import { getJoke } from "./getJoke"
-const jokeBtn = document.querySelector("#joke-btn")
+const jokeBtn = document.querySelector<HTMLElement>("#joke-btn")
+const jokeContainer = document.querySelector<HTMLElement>("#joke-container")
 
-jokeBtn?.addEventListener("click", async () => {
-  const joke = await getJoke()
-  console.log(joke)
-  jokeBtn.textContent = "Tell me another Chuck Norris joke"
-})
+const clickJokeBtn = () => {
+  jokeBtn?.addEventListener("click", async () => {
+    const joke = await getJoke()
+    if (jokeContainer) jokeContainer.textContent = joke
+    if (jokeBtn) jokeBtn.textContent = "Tell me another Chuck Norris joke"
+  })
+}
+clickJokeBtn()
