9b773fcfd4a7abf6c8b2a3211ecf020705025ab4
NehGuk <nurture.tumulus_0n@icloud.com>
2026-04-27 21:40:12 +0200

    Lays out main content structure on index.html

--- src/scripts/displayJoke.ts
+++ src/scripts/displayJoke.ts
@@ -4,4 +4,5 @@ const jokeBtn = document.querySelector("#joke-btn")
 jokeBtn?.addEventListener("click", async () => {
   const joke = await getJoke()
   console.log(joke)
+  jokeBtn.textContent = "Tell me another Chuck Norris joke"
 })
